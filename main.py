fe1a84a646b5224db01c2606f04c1ae680b24675
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2020-12-01 21:19:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# Importing libraries
+import pygame
+from pygame.locals import *
+
+# Initialising Pygame
+pygame.init()
+
+# Creating the screen and setting the dimensions
+world = pygame.display.set_mode([1080, 720])
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+
+# Creating the main class for entities in the game
+class Entity:
+
+    # Initialising the Class and setting each instance's attributes
+    def __init__(self, HPMax, MPMax, Strength, Magic, Defence, Resistance, Speed):
+        self.HPMax, self.HP = HPMax
+        self.MPMax, self.MP = MPMax
+        self.Strength = Strength
+        self.Magic = Magic
+        self.Defence = Defence
+        self.Resistance = Resistance
+        self.Speed = Speed
+
+    # Attack function [WIP]
+    def Attack(self):
+        pass
+
+
+# Opening Screen for the Game
+def main_Menu():
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.QUIT()
+
+        clock.tick(60)
+
+
+# Main game state
+def main_Game():
+    pass
+
+
+# Options Menu
+def options():
+    pass
+
+
+main_Menu()
