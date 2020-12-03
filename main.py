b60eae40e69c42e29a972189b3d446c592297c74
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2020-12-03 19:35:11 +0000

    Made a button that changes colour when the mouse hovers over it

--- main.py
+++ main.py
@@ -35,15 +35,35 @@ class Player(Entity):
         super().__init__(HPMax, MPMax, Strength, Magic, Defence, Resistance, Speed)
 
 
+def button(start_x, start_y, width, height, function):
+    if start_x + width > mouse[0] > start_x and start_y + height > mouse[1] > start_y:
+        pygame.draw.rect(world, (50, 255, 50), (start_x, start_y, width, height))
+    else:
+        pygame.draw.rect(world, (255, 255, 255), (start_x, start_y, width, height))
+
+
+def redraw_World():
+    world.fill((0, 0, 0))
+
+
 # Opening Screen for the Game
 def main_Menu():
+    global mouse, click
     while True:
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        redraw_World()
+
+        button(300, 450, 50, 50, None)
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
+        pygame.display.update()
         clock.tick(60)
 
 
