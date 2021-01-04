2e569e246b1d1bfb8e071db56b5eb8072f488aea
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2021-01-04 11:53:51 +0000

    Tried making another gamestate but currently crashes when trying to change

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pygame
 from pygame.locals import *
 import sys
 
-# Initialising Pygame
+# Initialising Pygame and the mixer
 pygame.init()
+pygame.mixer.pre_init(44100, -16, 2, 512)
 
 # Creating the screen and setting the dimensions
 world = pygame.display.set_mode([1280, 720])
@@ -14,8 +15,16 @@ clock = pygame.time.Clock()
 monitor_dimensions = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 fullscreen = False
 
+# Loading Images
 titleScreen = pygame.image.load('Images/Title Screen.png')
+mainScreen = pygame.image.load('Images/UI.png')
 titleScreen.convert()
+mainScreen.convert()
+
+# Defining variables for the mouse and clicks
+mouse = pygame.mouse.get_pos()
+click = pygame.mouse.get_pressed(num_buttons=5)
+gameState = 0
 
 
 # Creating the main class for entities in the game
@@ -41,6 +50,7 @@ class Player(Entity):
         super().__init__(HPMax, MPMax, Strength, Magic, Defence, Resistance, Speed)
 
 
+# Function for creating buttons
 def button(start_x, start_y, width, height, function):
     if start_x + width > mouse[0] > start_x and start_y + height > mouse[1] > start_y:
         pygame.mouse.set_cursor(*pygame.cursors.diamond)
@@ -51,10 +61,16 @@ def button(start_x, start_y, width, height, function):
         pygame.draw.rect(world, (0, 255, 0), (start_x, start_y, width, height))
 
 
+# Function for redrawing the graphics in the game
 def redraw_World():
     world.fill((0, 0, 0))
     bg = world.get_rect()
-    world.blit(titleScreen, bg)
+    if gameState == 0:
+        world.blit(titleScreen, bg)
+    elif gameState == 1:
+        world.blit(mainScreen, bg)
+    else:
+        pass
 
 
 # Opening Screen for the Game
@@ -94,11 +110,33 @@ def main_Menu():
 
 # Main game state
 def main_Game():
-    pass
+    global gameState, world, fullscreen
+    gameState = 1
+    while True:
+
+        redraw_World()
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == K_f:
+                    fullscreen = not fullscreen
+                    if fullscreen:
+                        world = pygame.display.set_mode(monitor_dimensions, pygame.FULLSCREEN)
+                    else:
+                        world = pygame.display.set_mode((world.get_width(), world.get_height()), pygame.SCALED)
 
 
 # Save Game
 def save_Game():
+    global gameState
+    gameState = 2
     pass
 
 
