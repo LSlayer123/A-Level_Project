e65dad4ef715edacb09a98597bcf7bdf5f7ff68a
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2021-02-20 22:42:27 +0000

    STAGE 2 MAP DONE

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ import random
 # Initialising Pygame and the mixer
 pygame.init()
 pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.mixer.music.load('Music.wav')
+pygame.mixer.music.play(-1, 0.0)
 
 # Creating the screen and setting the dimensions
 world = pygame.display.set_mode([1280, 720])
+frame = pygame.Surface([1280, 720])
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
 # Loading Images and converting them
 titleScreen = pygame.image.load('Images/Title Screen.png').convert()
-mainScreen = pygame.image.load('Images/UI.png').convert()
+mainScreen = pygame.transform.scale(pygame.image.load('Images/UI.png').convert(), (1280, 720))
 Blue = pygame.image.load('Images/Blue.png').convert()
 Green = pygame.image.load('Images/Green.png').convert()
 Orange = pygame.image.load('Images/Orange.png').convert()
@@ -29,6 +32,7 @@ gameState = 0
 resolution = 1
 smallFont = pygame.font.SysFont('Arial', 23)
 largeFont = pygame.font.SysFont('Arial', 36)
+playerLocation = [-1, -1]
 
 
 # Creating the main class for entities in the game
@@ -56,13 +60,12 @@ class Player(Entity):
 
 # Function for creating buttons
 def button(start_x, start_y, width, height, function, text):
-    start_x = (start_x / 1280) * world.get_width()
-    start_y = (start_y / 720) * world.get_height()
-    width = (width / 1280) * world.get_width()
-    height = (height / 720) * world.get_height()
-    if start_x + width > mouse[0] > start_x and start_y + height > mouse[1] > start_y:
+    if ((start_x / 1280) * world.get_width()) + ((width / 1280) * world.get_width()) > mouse[0] > \
+            ((start_x / 1280) * world.get_width()) and (start_y / 720) * world.get_height() \
+            + (height / 720) * world.get_height() > mouse[1] > (start_y / 720) * world.get_height():
         pygame.mouse.set_cursor(*pygame.cursors.diamond)
-        pygame.draw.rect(world, (150, 255, 0), (start_x, start_y, width, height))
+        pygame.draw.rect(frame, (150, 255, 0), (start_x, start_y, width, height))
+
         if click[0]:
             event = pygame.event.wait()
             if event.type == pygame.MOUSEBUTTONUP:
@@ -70,9 +73,9 @@ def button(start_x, start_y, width, height, function, text):
                 function()
 
     else:
-        pygame.draw.rect(world, (0, 255, 0), (start_x, start_y, width, height))
+        pygame.draw.rect(frame, (0, 255, 0), (start_x, start_y, width, height))
     title = smallFont.render(text, True, (0, 0, 255))
-    world.blit(title, (start_x, (start_y + (height / 2))))
+    frame.blit(title, (start_x, (start_y + (height / 2))))
 
 
 # Function for changing the resolution of the screen
@@ -85,7 +88,7 @@ def change_Resolution():
         resolution = 0
 
     if resolution == 0:
-        world = pygame.display.set_mode([640, 480])
+        world = pygame.display.set_mode([720, 576])
         smallFont = pygame.font.SysFont('Arial', 18)
         largeFont = pygame.font.SysFont('Arial', 25)
     elif resolution == 1:
@@ -98,17 +101,22 @@ def change_Resolution():
         largeFont = pygame.font.SysFont('Arial', 47)
 
 
-# Function for redrawing the graphics in the game
-def redraw_World():
+# Function for setting the background for the current game state
+def set_Background():
     global titleScreen, mainScreen
-    world.fill((0, 0, 0))
     bg = world.get_rect()
+
     if gameState == 0:
-        titleScreenCopy = pygame.transform.scale(titleScreen, (bg[2], bg[3]))
-        world.blit(titleScreenCopy, bg)
+        frame.blit(titleScreen, bg)
     elif gameState == 1:
-        mainScreenCopy = pygame.transform.scale(mainScreen, (bg[2], bg[3]))
-        world.blit(mainScreenCopy, bg)
+        frame.blit(mainScreen, bg)
+
+
+# Function for redrawing the assets to the displayed surface at the correct resolution
+def redraw_World():
+    world.fill((0, 0, 0))
+    frameCopy = pygame.transform.scale(frame, (world.get_width(), world.get_height()))
+    world.blit(frameCopy, frameCopy.get_rect())
 
 
 # Function for generating the layout of the floors in the game
@@ -122,16 +130,36 @@ def generate_Floor():
 
 # Function for rendering the map of the current floor on screen
 def render_Map(rooms):
+    global playerLocation
+
+    pygame.draw.rect(frame, [204, 153, 255], [17.5, 62, 40, 40])
+    pygame.draw.rect(frame, [204, 153, 255], [318.5, 62, 40, 40])
+
     for i in range(len(rooms)):
-        pygame.draw.rect(world, [204, 153, 255], [17.5, 62, 40, 40])
-        pygame.draw.rect(world, [204, 153, 255], [318.5, 62, 40, 40])
         for j in range(len(rooms[i])):
-            if rooms[i][j] == 2:
-                pygame.draw.rect(world, [255, 165, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
-            elif rooms[i][j] == 3:
-                pygame.draw.rect(world, [255, 0, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
-            elif rooms[i][j] == 4:
-                pygame.draw.rect(world, [0, 0, 255], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
+            if ((17.0 + (i * 45)) / 720) * world.get_height() + (40.0 / 720) * world.get_height() > mouse[1] > \
+                    ((17.0 + (i * 45)) / 720) * world.get_height() and (((60.5 + (j * 43)) / 1280) * world.get_width())\
+                    + ((40.0 / 1280) * world.get_width()) > mouse[0] > (((60.5 + (j * 43)) / 1280) * world.get_width()):
+                if rooms[i][j] == 2:
+                    pygame.draw.rect(frame, [255, 165, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40], True)
+                elif rooms[i][j] == 3:
+                    pygame.draw.rect(frame, [255, 0, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40], True)
+                elif rooms[i][j] == 4:
+                    pygame.draw.rect(frame, [0, 0, 255], [60.5 + (j * 43), 17 + (i * 45), 40, 40], True)
+
+                if click[0] and playerLocation[1] == j-1 and gameState == 1:
+                    playerLocation = [i, j]
+                    print(playerLocation)
+            else:
+                if rooms[i][j] == 2:
+                    pygame.draw.rect(frame, [255, 165, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
+                elif rooms[i][j] == 3:
+                    pygame.draw.rect(frame, [255, 0, 0], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
+                elif rooms[i][j] == 4:
+                    pygame.draw.rect(frame, [0, 0, 255], [60.5 + (j * 43), 17 + (i * 45), 40, 40])
+
+    if playerLocation != [-1, -1]:
+        pygame.draw.rect(frame, [0, 255, 0], [60.5 + (playerLocation[1] * 43), 17 + (playerLocation[0] * 45), 40, 40])
 
 
 # Opening Screen for the Game
@@ -142,7 +170,8 @@ def main_Menu():
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed(num_buttons=5)
 
-        redraw_World()
+        set_Background()
+
         pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
         button(100.0, 200.0, 100.0, 50.0, main_Game, 'Start')
@@ -160,20 +189,28 @@ def main_Menu():
                 if event.key == K_f:
                     pygame.display.toggle_fullscreen()
 
+        redraw_World()
+
         pygame.display.update()
         clock.tick(60)
 
 
 # Main game state
 def main_Game():
-    global gameState, world
+    global gameState, world, mouse, click, playerLocation
     gameState = 1
     floor = generate_Floor()
     print(floor)
+    playerLocation = [-1, -1]
     while gameState == 1:
 
-        redraw_World()
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed(num_buttons=5)
+
+        set_Background()
         render_Map(floor)
+
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -186,6 +223,7 @@ def main_Game():
                 if event.key == K_f:
                     pygame.display.toggle_fullscreen()
 
+        redraw_World()
         pygame.display.update()
         clock.tick(60)
 
