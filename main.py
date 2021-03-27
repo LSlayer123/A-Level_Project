a8c8534337570c828e9bbc3db6faca3077f6fa4d
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2021-03-27 13:30:30 +0000

    Stage 5: Character and Enemy creation basically complete

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 import sys
 import random
+import math
 
 # Initialising Pygame and the mixer
 pygame.init()
@@ -40,6 +41,7 @@ roomClear = False
 floorNumber = 0
 startTime = 0
 endTime = 0
+updateText = smallFont.render("", True, (0, 0, 0))
 
 
 # --------------------------------CLASSES------------------------------------------------------------------------------#
@@ -67,6 +69,8 @@ class Entity:
 class Player(Entity):
     def __init__(self, HPMax, MPMax, Strength, Magic, Defence, Resistance, Name):
         super().__init__(HPMax, MPMax, Strength, Magic, Defence, Resistance, Name)
+        self.Class = ""
+        self.statPoints = 5
 
     def Attack(self):
         global finalAttack
@@ -76,6 +80,45 @@ class Player(Entity):
         global finalAttack
         finalAttack = -1
         self.Defence += 5
+
+    def classChange(self, selection):
+
+        if selection == "warrior":
+            self.Strength += 10
+            self.Magic += 1
+            self.Defence += 5
+            self.Resistance += 3
+            self.Class = "Warrior"
+
+        elif selection == "mage":
+            self.Strength += 1
+            self.Magic += 10
+            self.Defence += 3
+            self.Resistance += 5
+            self.Class = "Mage"
+
+        elif selection == "rogue":
+            self.Strength += 6
+            self.Magic += 5
+            self.Defence += 3
+            self.Resistance += 2
+            self.Class = "Rogue"
+
+    def strengthUp(self):
+        self.Strength += 1
+        self.statPoints -= 1
+
+    def magicUp(self):
+        self.Magic += 1
+        self.statPoints -= 1
+
+    def defenceUp(self):
+        self.Defence += 1
+        self.statPoints -= 1
+
+    def resistanceUp(self):
+        self.Resistance += 1
+        self.statPoints -= 1
 
     def Ability1(self):
         pass
@@ -92,7 +135,7 @@ class Player(Entity):
 
 # ---------------------------------ADMIN FUNCTIONS---------------------------------------------------------------------#
 # Function for creating buttons
-def button(start_x, start_y, width, height, function, text):
+def button(start_x, start_y, width, height, function, text, parameter=None):
     if ((start_x / 1280) * world.get_width()) + ((width / 1280) * world.get_width()) > mouse[0] > \
             ((start_x / 1280) * world.get_width()) and (start_y / 720) * world.get_height() \
             + (height / 720) * world.get_height() > mouse[1] > (start_y / 720) * world.get_height():
@@ -101,9 +144,12 @@ def button(start_x, start_y, width, height, function, text):
 
         if click[0]:
             event = pygame.event.wait()
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP and function is not None:
                 pygame.mouse.set_cursor(*pygame.cursors.arrow)
-                function()
+                if parameter is not None:
+                    function(parameter)
+                else:
+                    function()
 
     else:
         pygame.draw.rect(frame, (0, 255, 0), (start_x, start_y, width, height))
@@ -198,16 +244,87 @@ def render_Map(rooms):
 
 # Character Creation Function
 def characterCreation():
-    User = Player(150, 50, 10, 10, 5, 5, "Player")
+    global gameState, world, mouse, click, floor, updateText
+    characterCreated = False
+    User = Player(150, 50, 0, 0, 0, 0, "Player")
+
+    while not characterCreated:
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed(num_buttons=5)
+
+        set_Background()
+
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
+
+        currentFloorNumber = largeFont.render(str(floorNumber), True, (0, 0, 0))
+        frame.blit(currentFloorNumber, (557.0, 75.0))
+
+        if User.Class == "":
+            button(30, 170, 80, 50, User.classChange, "Warrior", "warrior")
+            button(130, 170, 80, 50, User.classChange, "Mage", "mage")
+            button(230, 170, 80, 50, User.classChange, "Rogue", "rogue")
+        else:
+            button(30, 170, 80, 50, None, "Warrior")
+            button(130, 170, 80, 50, None, "Mage")
+            button(230, 170, 80, 50, None, "Rogue")
+
+        statPointText = smallFont.render("Stat Points remaining:" + str(User.statPoints), True, (0, 0, 0))
+        frame.blit(statPointText, (30, 240))
+
+        strengthText = smallFont.render("Strength:" + str(User.Strength), True, (0, 0, 0))
+        frame.blit(strengthText, (30, 270))
+        magicText = smallFont.render("Magic:" + str(User.Magic), True, (0, 0, 0))
+        frame.blit(magicText, (130, 270))
+        defenceText = smallFont.render("Defence:" + str(User.Defence), True, (0, 0, 0))
+        frame.blit(defenceText, (230, 270))
+        resistanceText = smallFont.render("Resistance:" + str(User.Resistance), True, (0, 0, 0))
+        frame.blit(resistanceText, (330, 270))
+
+        if User.statPoints != 0:
+            button(30, 300, 80, 50, User.strengthUp, "+")
+            button(130, 300, 80, 50, User.magicUp, "+")
+            button(230, 300, 80, 50, User.defenceUp, "+")
+            button(330, 300, 80, 50, User.resistanceUp, "+")
+        else:
+            button(30, 300, 80, 50, None, "+")
+            button(130, 300, 80, 50, None, "+")
+            button(230, 300, 80, 50, None, "+")
+            button(330, 300, 80, 50, None, "+")
+
+        updateText = smallFont.render("Your class is " + User.Class, True, (0, 0, 0))
+        frame.blit(updateText, (17.5, 545.0))
+
+        if User.Class != "" and User.statPoints == 0:
+            characterCreated = True
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == K_f:
+                    pygame.display.toggle_fullscreen()
+
+        redraw_World()
+        pygame.display.update()
+        clock.tick(60)
+
     return User
 
 
 # Enemy Creation Function
 def enemyCreation(number=None):
+    global floorNumber
+
     if number == 1:
-        enemy = Entity(100, 50, 10, 0, 5, 0, "Troll")
+        enemy = Entity(100 + (floorNumber * 5), 50, 10 + floorNumber, 0, 5 + floorNumber, 0, "Troll")
     else:
-        enemy = Entity(50, 50, 7, 2, 2, 2, "Golbin")
+        enemy = Entity(50 + (floorNumber * 5), 50, 7 + floorNumber, 2, 2 + (floorNumber / 5), 2 + (floorNumber / 5), "Golbin")
+
     return enemy
 
 
@@ -251,7 +368,7 @@ def main_Menu():
 
 # Main game state
 def main_Game():
-    global gameState, world, mouse, click, playerLocation, floor, roomClear, floorNumber, startTime
+    global gameState, world, mouse, click, playerLocation, floor, roomClear, floorNumber, startTime, updateText
     gameState = 1
 
     playerCharacter = characterCreation()
@@ -292,6 +409,8 @@ def main_Game():
         currentFloorNumber = largeFont.render(str(floorNumber), True, (0, 0, 0))
         frame.blit(currentFloorNumber, (557.0, 75.0))
 
+        frame.blit(updateText, (17.5, 545.0))
+
         if not roomClear:
             if floor[playerLocation[0]][playerLocation[1]] == 2 and playerLocation != [-1, -1]:
                 combat(playerCharacter, enemyCreation())
@@ -301,13 +420,18 @@ def main_Game():
                 Effect = random.randint(0, 4)
                 if Effect == 0:
                     playerCharacter.HP += 50
+                    if playerCharacter.HP > playerCharacter.HPMax:
+                        playerCharacter.HP = playerCharacter.HPMax
                     roomClear = True
+                    updateText = smallFont.render("You found a holy spring that heals 50HP.", True, (0, 0, 0))
                 elif Effect == 1:
                     playerCharacter.Strength += 2
                     roomClear = True
+                    updateText = smallFont.render("You found a demon fruit - raising your strength.", True, (0, 0, 0))
                 elif Effect == 2:
                     playerCharacter.Defence += 2
                     roomClear = True
+                    updateText = smallFont.render("You found a shield fruit - raising your defence.", True, (0, 0, 0))
                 elif Effect == 3:
                     combat(playerCharacter, enemyCreation())
                 elif Effect == 4:
@@ -335,13 +459,16 @@ def main_Game():
 
 # Function for the combat encounters in the game
 def combat(player, enemy):
-    global gameState, world, mouse, click, playerLocation, floor, finalAttack, enemyFinalAttack, roomClear, floorNumber\
-        , endTime
+    global gameState, world, mouse, click, playerLocation, floor, finalAttack, enemyFinalAttack, roomClear, floorNumber \
+        , endTime, updateText
     basePlayerDefence = player.Defence
     baseEnemyDefence = enemy.Defence
     enemyDamageText = smallFont.render("", True, (0, 0, 0))
     playerDamageText = smallFont.render("", True, (0, 0, 0))
     gameState = 2
+
+    updateText = smallFont.render("You encountered " + enemy.Name, True, (0, 0, 0))
+
     while enemy.HP > 0 and player.HP > 0:
         finalAttack = 0
         enemyFinalAttack = 0
@@ -393,6 +520,8 @@ def combat(player, enemy):
         currentFloorNumber = largeFont.render(str(floorNumber), True, (0, 0, 0))
         frame.blit(currentFloorNumber, (557.0, 75.0))
 
+        frame.blit(updateText, (17.5, 545.0))
+
         frame.blit(enemyDamageText, (17.5, 160.0))
         frame.blit(playerDamageText, (17.5, 180.0))
 
@@ -408,7 +537,7 @@ def combat(player, enemy):
             finalAttack -= enemy.Defence
             if finalAttack < 0:
                 finalAttack = 0
-            enemy.HP -= finalAttack
+            enemy.HP -= math.floor(finalAttack)
             enemyDamageText = smallFont.render("You did " + str(finalAttack) + " damage to " + enemy.Name, True,
                                                (0, 0, 0))
             if enemy.HP > 0:
@@ -416,7 +545,7 @@ def combat(player, enemy):
                 enemyFinalAttack -= player.Defence
                 if enemyFinalAttack < 0:
                     enemyFinalAttack = 0
-                player.HP -= enemyFinalAttack
+                player.HP -= math.floor(enemyFinalAttack)
                 playerDamageText = smallFont.render("You took " + str(enemyFinalAttack) + " damage from " + enemy.Name,
                                                     True, (0, 0, 0))
 
@@ -430,6 +559,7 @@ def combat(player, enemy):
     if enemy.HP <= 0:
         gameState = 1
         roomClear = True
+        updateText = smallFont.render("You beat " + enemy.Name, True, (0, 0, 0))
     else:
         gameState = 0
         endTime = pygame.time.get_ticks()
